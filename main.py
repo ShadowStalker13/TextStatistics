d8ae4fefdfa8560ef0bbab86e0991115b1b813c4
Andrew Puchkov <andr1502puch@gmail.com>
2021-03-22 22:42:21 +0300

    In main.py added 2 def

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+def get_file(filename):
+    with open(filename, encoding = "utf-8") as file:
+        text = file.read()
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    return words
+
+def words_into_dict(words):
+    words_dict = dict()
+    for word in words:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    return words_dict
