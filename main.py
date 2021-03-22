75edbe072583e45cd4977f370a95e3cc3ec2bac2
Andrew Puchkov <andr1502puch@gmail.com>
2021-03-22 22:53:08 +0300

    In main.py added def count_words

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-def get_file(filename):
+def get_words(filename):
     with open(filename, encoding = "utf-8") as file:
         text = file.read()
     text = text.replace("\n", " ")
@@ -18,3 +18,14 @@ def words_into_dict(words):
         else:
             words_dict[word] = 1
     return words_dict
+
+def count_words(filename):
+    words = get_words(filename)
+    words_dict = words_into_dict(words)
+    print("Количество слов: " % len(words))
+    print("Количество уникальных слов: " % len(words_dict))
+    print("Все использованные слова:")
+    for word in words_dict:
+        print(word.ljust(20), words_dict[word])
+
+
