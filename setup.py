2485da8dd6acb9fcd3c1491512f13550dc072653
Andrew Puchkov <andr1502puch@gmail.com>
2021-05-19 00:33:18 +0300

    WORKING VERSION 1.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TextStatistic",
-    version="1.0.3",
+    version="1.0.6",
     author="Andrew Puchkov",
     author_email="andr1502puch@gmail.com",
     description="Text statistics",
